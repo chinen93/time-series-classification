15455c140bc00a58d65225596c98496d8dcbdd74
Pedro Chinen <pedro.chinen@neuralmind.ai>
2021-03-16 13:21:39 -0300

    [feat] change FCN model to be more similar to the paper

--- src/project/ConvNet.py
+++ src/project/ConvNet.py
@@ -9,28 +9,54 @@ class ConvNet(nn.Module):
         self.n_in = n_in
         self.n_classes = n_classes
 
-        self.conv1 = nn.Conv2d(1, 128, (7, 1), 1, (3, 0))
-        self.bn1 = nn.BatchNorm2d(128)
+        self.conv1 = nn.Conv1d(
+            in_channels=1,
+            out_channels=128,
+            kernel_size=8,
+            stride=1,
+            padding=3,
+            padding_mode='replicate'
+        )
+        self.bn1 = nn.BatchNorm1d(num_features=128)
 
-        self.conv2 = nn.Conv2d(128, 256, (5, 1), 1, (2, 0))
-        self.bn2 = nn.BatchNorm2d(256)
+        self.conv2 = nn.Conv1d(
+            in_channels=128,
+            out_channels=256,
+            kernel_size=5,
+            stride=1,
+            padding=2,
+            padding_mode='replicate'
+        )
+        self.bn2 = nn.BatchNorm1d(num_features=256)
 
-        self.conv3 = nn.Conv2d(256, 128, (3, 1), 1, (1, 0))
-        self.bn3 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv1d(
+            in_channels=256,
+            out_channels=128,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            padding_mode='replicate'
+        )
+        self.bn3 = nn.BatchNorm1d(num_features=128)
 
-        self.fc4 = nn.Linear(128, self.n_classes)
+        self.conv4 = nn.Conv1d(
+            in_channels=128,
+            out_channels=self.n_classes,
+            kernel_size=1
+        )
+        self.avgpool = nn.AdaptiveAvgPool1d(output_size=1)
+
 
 
     def forward(self, x: torch.Tensor):
-        x = x.view(-1, 1, self.n_in, 1)
+        x = x.view(-1, 1, self.n_in)
 
         x = F.relu(self.bn1(self.conv1(x)))
         x = F.relu(self.bn2(self.conv2(x)))
         x = F.relu(self.bn3(self.conv3(x)))
 
-        x = F.avg_pool2d(x, (2, 1))
-        x = torch.mean(x, dim=2)
-        x = x.view(-1, 128)
-        x = self.fc4(x)
+        x = self.conv4(x)
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
 
         return x
